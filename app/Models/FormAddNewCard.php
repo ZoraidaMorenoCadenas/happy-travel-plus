38f746aaecfa7e9b36dd754304c38b37bd108291
Sylvia Llorente <sylviall81@gmail.com>
2023-08-30 15:23:51 +0200

    pruebas bearer token para auth en api

--- app/Models/FormAddNewCard.php
+++ app/Models/FormAddNewCard.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+/*namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -23,4 +23,4 @@ class FormAddNewCard extends Model
     }
 
 
-}
+}*/
--- routes/api.php
+++ routes/api.php
@@ -21,24 +21,6 @@ use App\Http\Controllers\CardController;
 
 Route::resource('cards', CardController::class);
  
-
-/*Route::get('/cards', [CardController::class, 'index']);
-Route::post('/cards', [CardController::class, 'store']);
-Route::get('/cards/{id}', [CardController::class, 'show']);
-Route::put('/cards/{id}', [CardController::class, 'update']);
-Route::delete('/cards/{id}', [CardController::class, 'destroy']);*/
-
-
-/*
-Route::get('/cards', [CardController::class, 'index']);
-Route::get('/cards/{id}', [CardController::class, 'show']);
-Route::post('/cards', [CardController::class, 'store']);
-Route::put('/cards/{id}', [CardController::class, 'update']);
-Route::delete('/cards/{id}', [CardController::class, 'destroy']);*/
-
-
-
-
 //Route::apiResource('happy_travel', TravelController::class);
 
 // // GET All destination http://localhost:8000/api/happy_travel
